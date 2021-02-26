68282226771e46c5bb32297cf96df835492557f2
arushivyas <pittsburgh@pittsburgh.lan>
2021-02-26 11:17:17 -0500

    version bump 1.0.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(path.join(path.dirname(path.abspath(__file__)), 'README.rst')) as f:
 
 setup(
     name             = 'pfdo_med2img',
-    version          = '1.0.0',
+    version          = '1.0.2',
     description      = 'An app to recursively walk down a directory tree and perform a med2image on files in each directory.',
     long_description = readme,
     author           = 'Arushi Vyas',
